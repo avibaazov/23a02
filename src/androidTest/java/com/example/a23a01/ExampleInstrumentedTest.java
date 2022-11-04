0e951ca18f4e3cdb6347195f58016e56cd16794b
avibaazov <avibaazov2@gmail.com>
2022-11-04 02:36:02 +0200

    init project

--- src/androidTest/java/com/example/a23a01/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/example/a23a01/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
+
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.a23a01", appContext.getPackageName());
     }
